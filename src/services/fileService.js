46de54438d415a270d3328ad772916d2e38d97e4
anh0512 <thuvu0512@gmail.com>
2023-12-06 14:56:23 +0000

    add image functions to fileService

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -2,6 +2,7 @@ import uuid from 'react-native-uuid';
 import * as FileSystem from 'expo-file-system';
 
 const contactDirectory = `${FileSystem.documentDirectory}contacts`;
+const imageDirectory = `${FileSystem.documentDirectory}images`
 
 const onException = (cb, errorHandler) => {
   try {
@@ -16,6 +17,11 @@ const onException = (cb, errorHandler) => {
 
 export const cleanDirectory = async () => {
   await FileSystem.deleteAsync(contactDirectory);
+  await FileSystem.deleteAsync(imageDirectory)
+};
+
+export const writeAsStringFile = async (file, contents) => {
+  return await onException(() => FileSystem.writeAsStringAsync(file, contents));
 };
 
 export const copyFile = async (file, newLocation) => {
@@ -25,12 +31,7 @@ export const copyFile = async (file, newLocation) => {
   }));
 };
 
-export const writeAsStringFile = async (file, contact) => {
-  return await onException(() => FileSystem.writeAsStringAsync(file, contact));
-};
-
 export const addContact = async contact => {
-  await setupDirectory()
   const fileName = contact.name.toLowerCase() + '-' + uuid.v4() + '.json';
   await onException(() => writeAsStringFile(`${contactDirectory}/${fileName}`, JSON.stringify(contact)));
 
@@ -41,18 +42,45 @@ export const addContact = async contact => {
   };
 };
 
-export const remove = async name => {
+export const addImage = async imageLocation => {
+  const folderSplit = imageLocation.split('/');
+  const fileName = folderSplit[folderSplit.length - 1];
+  await onException(() => copyFile(imageLocation, `${imageDirectory}/${fileName}`));
+
+  return {
+    name: fileName,
+    type: 'image',
+    file: await loadImage(fileName)
+  };
+}
+
+export const removeContact = async name => {
   return await onException(() => FileSystem.deleteAsync(`${contactDirectory}/${name}`, { idempotent: true }));
 };
+
+export const removeImage = async name => {
+  return await onException(() => FileSystem.deleteAsync(`${imageDirectory}/${name}`, { idempotent: true }));
+}
 
 export const loadContact = async fileName => {
   return await JSON.parse(await onException(() => FileSystem.readAsStringAsync(`${contactDirectory}/${fileName}`)));
 };
 
+export const loadImage = async fileName => {
+  return await onException(() => FileSystem.readAsStringAsync(`${imageDirectory}/${fileName}`, {
+    encoding: FileSystem.EncodingType.Base64
+  }));
+}
+
 const setupDirectory = async () => {
-  const dir = await FileSystem.getInfoAsync(contactDirectory);
-  if (!dir.exists) {
+  const contactDir = await FileSystem.getInfoAsync(contactDirectory);
+  const imageDir = await FileSystem.getInfoAsync(imageDirectory);
+
+  if (!contactDir.exists) {
     await FileSystem.makeDirectoryAsync(contactDirectory);
+  }
+  if (!imageDir.exists) {
+    await FileSystem.makeDirectoryAsync(imageDirectory);
   }
 };
 
@@ -68,3 +96,16 @@ export const getAllContacts = async () => {
     };
   }));
 };
+
+export const getAllImages = async () => {
+  await setupDirectory();
+
+  const result = await onException(() => FileSystem.readDirectoryAsync(imageDirectory));
+  return Promise.all(result.map(async fileName => {
+    return {
+      name: fileName,
+      type: 'image',
+      file: await loadImage(fileName)
+    };
+  }));
+}
